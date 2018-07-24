81e41ec9e22c5fae0dc7fb99e38db156294cb40f
Chris Gantz <chrisgantz836@gmail.com>
2018-07-24 13:22:16 -0700

    added API module

---
+++ scripts/Api.js
@@ -0,0 +1,20 @@
+'use strict';
+/* global $ */
+
+const api = (function(){
+  const BASE_URL = ' https://www.googleapis.com/youtube/v3/search';
+
+  const API_KEY = 'AIzaSyDAVHHn1DQ_fc28Hj4KTJhgagEbLVO1MNY';
+
+  const fetchVideos = function(searchTerm, callback) {
+    const query = {
+      part: 'snippet',
+      key: API_KEY, 
+      q: searchTerm,
+    };
+    $.getJSON(BASE_URL, query, callback);
+  };
+  return {
+    fetchVideos
+  };
+}());
